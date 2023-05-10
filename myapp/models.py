7f2c71ac9aeeae79a168c9b80f8a785f0648ef99
amir <amir.yeganehk83@gmail.com>
2023-05-10 04:58:52 +0430

    new add

--- myapp/models.py
+++ myapp/models.py
@@ -2,6 +2,5 @@ from django.db import models
 
 class pro(models.Model):
     name = models.CharField(max_length=50)
-    LastName = models.CharField(max_length=50)
     create = models.DateTimeField ()
     expir = models.DateTimeField()
