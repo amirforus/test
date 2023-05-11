04b8fd0460ecf96b22146c60117cf6bd22c09baf
algonouir <algo.mahdi.nouri@gmail.com>
2023-05-11 14:30:04 +0330

    test1

--- myapp/models.py
+++ myapp/models.py
@@ -1,4 +1,17 @@
 from django.db import models
-
-class pro(models.Model):
+from rest_framework.serializers import ModelSerializer
+class First(models.Model):
     name = models.CharField(max_length=50)
+    price = models.IntegerField()
+
+    def __str__(self):
+        return (f"name -> {self.name} | price -> {self.price}")
+    
+
+class FirsSerializer(ModelSerializer):
+    class Meta:
+        model = First
+        fields = ['name', 'price']
+
+
+
